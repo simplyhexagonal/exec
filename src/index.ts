6af3327c2efc4bd24a22667c6c00522a523a8046
Jean M. Lescure <jeanmlescure@gmail.com>
2022-01-25 15:57:43 -0600

    chore: ensure js compatibility naming default export

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const realtimeLog = (...args: any[]) => {
   }
 }
 
-export default async (command: string) => {
+const exec = async (command: string) => {
   const logger = MonoContext.getStateValue('logger') || console;
 
   const child = execCallback(command);
@@ -107,3 +107,5 @@ export default async (command: string) => {
     )
   );
 };
+
+export default exec;
